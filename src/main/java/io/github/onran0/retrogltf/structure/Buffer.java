22f31440c1dc8a6499e0d2ac37b5be0a0fa31be8
Onran <kotshel2009@gmail.com>
2026-08-07 06:52:08 +0900

    added classes for buffer & views

---
+++ src/main/java/io/github/onran0/retrogltf/structure/Buffer.java
@@ -0,0 +1,21 @@
+package io.github.onran0.retrogltf.structure;
+
+import org.json.JSONObject;
+
+public class Buffer {
+    private final int byteLength;
+    private final String uri;
+
+    public Buffer(JSONObject json) {
+        this.byteLength = json.getInt("byteLength");
+        this.uri = json.optString("uri");
+    }
+
+    public int getByteLength() {
+        return byteLength;
+    }
+
+    public String getURI() {
+        return uri;
+    }
+}
---
+++ src/main/java/io/github/onran0/retrogltf/structure/BufferView.java
@@ -0,0 +1,41 @@
+package io.github.onran0.retrogltf.structure;
+
+import org.json.JSONObject;
+
+public class BufferView {
+    private final int buffer;
+    private final int byteLength;
+    private final int byteOffset;
+    private final int byteStride;
+
+    public BufferView(JSONObject json) {
+        this.buffer = json.getInt("buffer");
+        this.byteLength = json.getInt("byteLength");
+        this.byteOffset = json.optInt("byteOffset", 0);
+        this.byteStride = json.optInt("byteStride", 1);
+    }
+
+    // getters
+
+    public int getBuffer() {
+        return buffer;
+    }
+
+    public int getByteLength() {
+        return byteLength;
+    }
+
+    public int getByteOffset() {
+        return byteOffset;
+    }
+
+    public int getByteStride() {
+        return byteStride;
+    }
+
+    // other
+
+    public int getOffsetInBuffer() {
+        return byteOffset;
+    }
+}
