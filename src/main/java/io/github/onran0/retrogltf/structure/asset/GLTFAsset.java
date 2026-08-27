93052ee49b43df621df2ff5351e83d466c1c5f8a
Onran <kotshel2009@gmail.com>
2026-08-27 21:15:52 +0900

    fix of asset.minVersion requiring

--- src/main/java/io/github/onran0/retrogltf/structure/asset/GLTFAsset.java
+++ src/main/java/io/github/onran0/retrogltf/structure/asset/GLTFAsset.java
@@ -2,17 +2,39 @@ package io.github.onran0.retrogltf.structure.asset;
 
 import org.json.JSONObject;
 
+import java.util.Optional;
+
 public class GLTFAsset {
     private final String copyright;
     private final String generator;
     private final String version;
     private final String minVersion;
 
+    private final int minorVersion;
+    private final int majorVersion;
+    private final Integer minorMinVersion;
+    private final Integer majorMinVersion;
+
     public GLTFAsset(JSONObject json) {
         this.copyright = json.optString("copyright");
         this.generator = json.optString("generator");
         this.version = json.getString("version");
-        this.minVersion = json.optString("minVersion");
+        this.minVersion = json.optString("minVersion", null);
+
+        String[] splitVersion = version.split("\\.");
+
+        this.minorVersion = Integer.parseInt(splitVersion[0]);
+        this.majorVersion = Integer.parseInt(splitVersion[1]);
+
+        if(this.minVersion != null) {
+            String[] splitMinVersion = minVersion.split("\\.");
+
+            minorMinVersion = Integer.parseInt(splitMinVersion[0]);
+            majorMinVersion = Integer.parseInt(splitMinVersion[1]);
+        } else {
+            this.minorMinVersion = null;
+            this.majorMinVersion = null;
+        }
     }
 
     public String getCopyright() {
@@ -27,7 +49,23 @@ public class GLTFAsset {
         return version;
     }
 
-    public String getMinVersion() {
-        return minVersion;
+    public int getMinorVersion() {
+        return minorVersion;
+    }
+
+    public int getMajorVersion() {
+        return majorVersion;
+    }
+
+    public Optional<String> getMinVersion() {
+        return Optional.of(minVersion);
+    }
+
+    public int getMinorMinVersion() {
+        return minorMinVersion;
+    }
+
+    public int getMajorMinVersion() {
+        return majorMinVersion;
     }
 }
